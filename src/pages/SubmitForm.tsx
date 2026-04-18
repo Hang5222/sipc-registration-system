9c72db38e1b2a0980b38946cb3191d8682ae17f6
hwh <1285224844@qq.com>
2026-04-18 13:04:51 +0800

    feat: 封装 axios 请求和响应拦截器，创建页面框架

---
+++ src/pages/SubmitForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SubmitForm: React.FC = () => {
+  return (
+    <div>
+      <h1>提交表单</h1>
+    </div>
+  )
+}
+
+export default SubmitForm
---
+++ src/utils/request.ts
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { message } from "antd";
+
+// 拦截器
+// 创建axios实例
+const request = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL || 'https://frontest.sipc115.com',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// 请求拦截器
+request.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token && config.headers) {
+      config.headers.Authorization = token; // 文档原文：Authorization: <token>
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+request.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+
+    // res.code业务状态码判断
+    // 成功
+    if (res.code === '200') {
+      return res
+    }
+    // 失败
+    if (res.code === 'A0400') {
+      message.error(`参数错误(A0400): ${res.message || '请检查表单'}`);
+    }
+    else if (res.code === 'A0500') {
+      message.error(`服务器错误(A0500): ${res.message || '请联系管理员'}`);
+    }
+    else {
+      message.error(res.msg || '未知错误');
+    }
+    return Promise.reject(new Error(res.message || 'Error')); // 虽然 HTTP 是 200，但业务失败了。手动把它转化成一个 Promise 拒接状态
+  },
+
+  (error) => {
+    // 处理http错误
+    if (error.response) {
+       message.error(`网络请求错误: HTTP ${error.response.status}`);
+    } else if (error.request) {
+      message.error('网络连接失败，请检查网络设置或稍后重试');
+    } else {
+      message.error('未知错误');
+    }
+    return Promise.reject(error);
+  }
+)
+
+export default request;
